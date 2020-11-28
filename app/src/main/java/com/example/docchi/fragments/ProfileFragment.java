69d9cf78f3cfcd8913637bef7d0720725f5e969b
Rohan <35950830+rohan328@users.noreply.github.com>
2020-11-28 12:43:57 +0530

    bug fixes
    fixed nullpointer in signupactivity and finish() after logout

--- app/src/main/java/com/example/docchi/fragments/ProfileFragment.java
+++ app/src/main/java/com/example/docchi/fragments/ProfileFragment.java
@@ -14,6 +14,7 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import com.example.docchi.LoginActivity;
+import com.example.docchi.MainActivity;
 import com.example.docchi.Post;
 import com.example.docchi.R;
 import com.parse.FindCallback;
@@ -83,6 +84,7 @@ public class ProfileFragment extends Fragment {
        if(ParseUser.getCurrentUser() == null){
             Intent i = new Intent(getActivity(), LoginActivity.class);
             startActivity(i);
+         ((MainActivity) getContext()).finish();
         }
     }
 
