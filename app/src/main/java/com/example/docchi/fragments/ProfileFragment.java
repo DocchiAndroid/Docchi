0fb1e139fe83afb2f9f1c518f6491d9325bca80b
Kijimj7 <pinkokinawa84@gmail.com>
2020-12-06 20:06:34 -0800

    Changed profile page design

--- app/src/main/java/com/example/docchi/fragments/ProfileFragment.java
+++ app/src/main/java/com/example/docchi/fragments/ProfileFragment.java
@@ -23,6 +23,7 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.load.resource.bitmap.CircleCrop;
+import com.bumptech.glide.request.RequestOptions;
 import com.example.docchi.AboutActivity;
 import com.example.docchi.HelpActivity;
 import com.example.docchi.LoginActivity;
@@ -179,7 +180,7 @@ public class ProfileFragment extends Fragment {
       if(file != null){
         Glide.with(this).
                 load(file.getUrl()).
-                transform(new CircleCrop()).
+                apply(RequestOptions.circleCropTransform()).
                 into(ivProfilePic);
       } else {
         Glide.with(this).
