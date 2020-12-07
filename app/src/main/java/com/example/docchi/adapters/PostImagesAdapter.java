43de2078f052b523d6a974f2e46e8bd443eab7b2
Kijimj7 <pinkokinawa84@gmail.com>
2020-12-06 16:07:06 -0800

    Changed vote image and added animation

--- app/src/main/java/com/example/docchi/adapters/PostImagesAdapter.java
+++ app/src/main/java/com/example/docchi/adapters/PostImagesAdapter.java
@@ -61,27 +61,29 @@ public class PostImagesAdapter extends RecyclerView.Adapter<PostImagesAdapter.My
 
     public class MyView extends RecyclerView.ViewHolder {
         ImageView ivImage;
-//        ImageView voteImage;
+        ImageView voteImage;
         TextView tvCount;
-//        AnimatedVectorDrawableCompat avd;
-//        AnimatedVectorDrawable avd2;
+        TextView btnVote;
+        AnimatedVectorDrawableCompat avd;
+        AnimatedVectorDrawable avd2;
 
         public MyView(@NonNull View itemView) {
             super(itemView);
             ivImage = itemView.findViewById(R.id.ivImage);
-//            voteImage = itemView.findViewById(R.id.ivVote);
+            voteImage = itemView.findViewById(R.id.ivVote);
+            btnVote = itemView.findViewById(R.id.btnVote);
             tvCount = itemView.findViewById(R.id.tvCount);
-            SparkButton spark = itemView.findViewById(R.id.ivVote);
 
-//            ((Activity) itemView.getContext()).getWindow().getDecorView().setSystemUiVisibility(
-//                    View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-//                            | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-//                            | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-//                            | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
-//                            | View.SYSTEM_UI_FLAG_FULLSCREEN
-//                            | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
-//
-//            );
+
+            ((Activity) itemView.getContext()).getWindow().getDecorView().setSystemUiVisibility(
+                    View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                            | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                            | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                            | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                            | View.SYSTEM_UI_FLAG_FULLSCREEN
+                            | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+
+            );
         }
 
         public void bind(int position){
@@ -95,11 +97,11 @@ public class PostImagesAdapter extends RecyclerView.Adapter<PostImagesAdapter.My
                 Glide.with(context).load(imageFile.getUrl()).into(ivImage);
             }
 
-            spark.setEventListener(new SparkEventListener(){
-                @Override
-                public void onEvent(ImageView button, boolean buttonState) {
-                    int pos = post.previousVote(username);
 
+            btnVote.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    int pos = post.previousVote(username);
                     if(pos != -1 && pos != position){
                         Toast.makeText(context, "You have already voted " + pos, Toast.LENGTH_SHORT).show();
                         return;
@@ -110,27 +112,16 @@ public class PostImagesAdapter extends RecyclerView.Adapter<PostImagesAdapter.My
                     //update ui
                     tvCount.setText(Integer.toString(image.getCount()));
 
-//                    Drawable drawable = voteImage.getDrawable();
-//
-//                    if(drawable instanceof AnimatedVectorDrawableCompat){
-//                        avd = (AnimatedVectorDrawableCompat) drawable;
-//                        avd.start();
-//                    }else if(drawable instanceof AnimatedVectorDrawable)
-//                        avd2 = (AnimatedVectorDrawable) drawable;
-//                    avd2.start();
-
-                }
-
-                @Override
-                public void onEventAnimationEnd(ImageView button, boolean buttonState) {
-
-                }
-
-                @Override
-                public void onEventAnimationStart(ImageView button, boolean buttonState) {
-
+                    Drawable drawable = voteImage.getDrawable();
+                    if(drawable instanceof AnimatedVectorDrawableCompat){
+                        avd = (AnimatedVectorDrawableCompat) drawable;
+                        avd.start();
+                    }else if(drawable instanceof AnimatedVectorDrawable)
+                        avd2 = (AnimatedVectorDrawable) drawable;
+                    avd2.start();
                 }
             });
+
         }
     }
 }
